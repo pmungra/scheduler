4e8e68c19b9f82185aa4acd6a9cd9fe18d6ce7a1
Your Name <parth.mungra94@gmail.com>
2022-02-17 22:00:02 +0000

    fixed error and added onSubmit

--- src/components/appointment/form.js
+++ src/components/appointment/form.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
@@ -7,7 +6,7 @@ export default function Form(props) {
     const [student, setStudent] = useState(props.student || "");
     const [interviewer, setInterviewer] = useState(props.interviewer || null);
     const reset = () => {
-        setName("");
+        setStudent("");
         setInterviewer(null);
       }
     
@@ -19,14 +18,14 @@ export default function Form(props) {
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
-    <form autoComplete="off">
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
     <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={name}
-            onChange={event => setName(event.target.value)}
+            value={student}
+            onChange={event => setStudent(event.target.value)}
           />
     </form>
     <InterviewerList 
@@ -37,7 +36,7 @@ export default function Form(props) {
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-    <Button danger onClick={props.onCancel}>Cancel</Button>
+    <Button danger onClick={cancel}>Cancel</Button>
     <Button confirm onClick={props.onSave}>Save</Button>
     </section>
   </section>
