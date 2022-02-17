46740b5144812e750ffed62d0052afc6757254f4
Your Name <parth.mungra94@gmail.com>
2022-02-17 22:31:03 +0000

    revised logic

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,14 +4,51 @@ export const getAppointmentsForDay = (state, day) => {
   if (!dayObj) {
     return [];
   }
-const appointmentID = dayObj.appointments;
-const appointmentsForEachDay = [];
+  const appointmentID = dayObj.appointments;
+  const appointmentsForEachDay = [];
 
   for (const id in state.appointments) {
     if (appointmentID.includes(Number(id))) {
       appointmentsForEachDay.push(state.appointments[id]);
     }
   }
-
   return appointmentsForEachDay;
+};
+
+
+export const getInterviewersForDay = (state, day) => {
+  const dayObj = state.days.find(elem => elem.name === day);
+
+  if (!dayObj) {
+    return [];
+  }
+
+  const interviewerID = dayObj.interviewers;
+
+  const interviewersForEachDay = [];
+
+  for (const id in state.interviewers) {
+    if (interviewerID.includes(Number(id))) {
+      interviewersForEachDay.push(state.interviewers[id]);
+    }
+  }
+
+  return interviewersForEachDay;
+};
+
+export const getInterview = (state, interview) => {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerID = interview.interviewer;
+
+  for (const id in state.interviewers) {
+    if (Number(id) === interviewerID) {
+      return {
+        student: interview.student,
+        interviewer: state.interviewers[id]
+      };
+    }
+  }
 };
