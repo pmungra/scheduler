c6d009df9ad8b7743cbce88d7f80839299372d51
Your Name <parth.mungra94@gmail.com>
2022-02-17 23:33:08 +0000

    added modes for show & empty

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -1,25 +1,34 @@
 import React from 'react'
+import useVisualMode from "hooks/useVisualMode";
 import Header from "./header";
 import Show from "./show";
 import Empty from "./empty";
 
+
+
 import "./styles.scss";
+
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 
+
+
 export default function Appointment(props) {
+    const { mode, transition, back } = useVisualMode(
+        props.interview ? SHOW : EMPTY
+      );
+  
     return (
       <article className="appointment">
         <Header time={props.time} />
-        {
-        props.interview ?
+        {mode === EMPTY && <Empty/>}
+        {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        /> :
-        <Empty/>
-        }
+          /> 
+      )}
       </article>
     )
 }
