a0cdb3cfb5eb92c09f4785fa0a327aa6e2b2cdaa
Your Name <parth.mungra94@gmail.com>
2022-02-17 23:36:59 +0000

    fixed errors

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  // Build and array of interviewer list items and render
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {props.interviewers.map((interviewer) => (
+          <InterviewerListItem
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            setInterviewer={(event) => props.onChange(interviewer.id)}
+            selected={interviewer.id === props.value}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+}
