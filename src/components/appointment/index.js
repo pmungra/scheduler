cf05c1291627a44a0b0bd371c8c9d622ffb4853b
Your Name <parth.mungra94@gmail.com>
2022-02-17 22:01:23 +0000

    imported header and added fragments

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { Fragment } from 'react'
 import "./styles.scss";
+
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+
 
 export default function Appointment(props) {
     return (
