22abba35ed9bc8cd72138b1b3baf236764d17724
Your Name <parth.mungra94@gmail.com>
2022-02-17 18:59:23 +0000

    added Dayllist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import DayListItem from "components/DayListItem";
 
 import "index.scss";
 
-import Application from "components/Application";
+import Button from "components/Button";
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+storiesOf("Button", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Base", () => <Button>Base</Button>)
+  .add("Confirm", () => <Button confirm>Confirm</Button>)
+  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Clickable", () => (
+    <Button onClick={action("button-clicked")}>Clickable</Button>
+  ))
+  .add("Disabled", () => (
+    <Button disabled onClick={action("button-clicked")}>
+      Disabled
+    </Button>
+  ));
