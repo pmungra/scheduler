5d346150539cb9b0cd6f60bbf479883cfb02bcfc
Your Name <parth.mungra94@gmail.com>
2022-02-18 00:52:12 +0000

    major errors fixed

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,31 @@
 import React from "react";
+import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+    const dayClass = classNames("day-list__item", {
+        "day-list__item--selected" : props.selected,
+        "day-list__item--full": props.spots === 0
+      })
+
+      const formatSpots = (numOfSpots) => {
+        switch(numOfSpots) {
+          case 0:
+            return 'no spots remaining';
+          case 1:
+            return '1 spot remaining';
+          default:
+            return `${numOfSpots} spots remaining`
+        }
+      }
   return (
-        <li onClick={() => props.setDay(props.name)}>
+    <li 
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
         <h2 className="text--regular">{props.name}</h2>
-        <h3 className="text--light">{props.spots} spots remaining</h3>
+        <h3 className="text--light">{formatSpots(props.spots)}</h3>
       </li>
     );
   }
